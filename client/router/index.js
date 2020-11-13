7c3084f5b71664886ee4ee806ec89c4ec6b2ccfe
Lucas <lucasgpiccinini@outlook.com.br>
2020-11-13 20:31:14 -0300

    criacao pagina logine rota de redirecionamento

--- client/router/index.js
+++ client/router/index.js
@@ -1,14 +1,19 @@
-import Vue from "vue"
-import VueRouter from "vue-router"
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  
+    {
+        path: '*',
+        name: 'Login',
+        component: Login,
+    },
 ]
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes,
 })
---
+++ client/views/Login.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-container>
+    <v-row align="center" justify="center" style="height: 100%">
+      <v-col cols="12" lg="4" xl="3" align-self="center">
+        <v-card elevation="10" class="pa-4" shaped>
+          <v-overlay :value="loading">
+            <v-progress-circular indeterminate size="64"></v-progress-circular>
+          </v-overlay>
+          <v-list-item>
+            <v-img :src="logo" />
+          </v-list-item>
+          <v-divider></v-divider>
+          <v-card-text>
+            <v-form ref="form" v-model="valid">
+              <v-text-field
+                prepend-icon="mdi-email"
+                label="Email"
+                type="email"
+                :rules="[rules.required, rules.email]"
+                v-model="access.email"
+                required
+              />
+              <v-text-field
+                prepend-icon="mdi-lock"
+                label="Password"
+                type="password"
+                :rules="[rules.required]"
+                v-model="access.password"
+                @keypress.enter="getIn()"
+                required
+              />
+              <v-btn
+                rounded
+                class="success mt-4"
+                block
+                :disabled="!valid"
+                @click="getIn()"
+              >
+                Entrar
+              </v-btn>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-snackbar v-model="error" color="error" top right>
+        Invalid email or password!
+    </v-snackbar>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+      loading: false,
+      valid: false,
+      access: {
+        email: "",
+        password: "",
+      },
+      rules: {
+        required: (val) => !!val || "Must be filled!",
+        email: (val) => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(val) || "Invalid email!";
+        },
+      },
+      logo: require("../src/assets/logo.png"),
+      error: null,
+    };
+  },
+  methods: {
+    getIn() {},
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  height: 100vh;
+}
+.v-image {
+  left: 10px;
+}
+</style>
