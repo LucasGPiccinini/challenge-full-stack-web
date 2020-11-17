6e4195c5a79e18539548fecf1e699f6a983f6888
Lucas <lucasgpiccinini@outlook.com.br>
2020-11-17 00:23:46 -0300

    modificado para alterar somente campos editaveis

--- server/src/App/Users/updateUser.js
+++ server/src/App/Users/updateUser.js
@@ -1,18 +1,18 @@
 'use strict'
 
+const { loadUserByEmail } = require("./loadUserByEmail")
+
 const sql = `
 UPDATE users_grupoa
    SET name = $1,
-       email = $2,
-       cpf = $3,
-       password = $4,
-       admin = $5,
-       token = $6
- WHERE id = $7
+       email = $2
+ WHERE id = $3
 `
 module.exports.updateUser = async (conn, data) => {
     try {
-        const user = await conn.query(sql, [data.name, data.email, data.cpf, data.password, data.admin, data.token, data.id])
+        let user = await loadUserByEmail(conn, data.email)
+        if (user.id && user.id != data.user_id) throw new Error(`Email ${data.email}, already exists!`)
+        user = await conn.query(sql, [data.name, data.email, data.user_id])
         return {
             message: ` Usuário ${data.name ? (data.name + ' ') : ''}alterado com sucesso! `,
             data: user.rows
